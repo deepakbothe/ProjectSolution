638fc698b188c4b2bc99835f2787cc0cdc1e196e
User <User@DESKTOP-P8RS130>
2021-08-25 17:28:00 +0530

    Add project files.

---
+++ ProjectSolution1/Controllers/DynamicBlogCardController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectSolution1.Models;
+using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Resources.Media;
+
+namespace ProjectSolution1.Controllers
+{
+    public class DynamicBlogCardController : Controller
+    {
+
+        // GET: DynamicBlogCard
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getBlogCard(FormCollection form)
+        {
+            string query = form["searchInput"];
+            List<BlogCard> BC = new List<BlogCard>();
+            var item = Sitecore.Context.Database.GetItem("{5713C618-0FA5-44F0-8B14-8A963031E2A6}");
+            Sitecore.Data.Fields.MultilistField BlogList = item.Fields["BlogList"];
+            if (BlogList != null)
+            {
+                foreach (Sitecore.Data.Items.Item I in BlogList.GetItems())
+                {
+                    var ID = I.ID.ToString();
+                    var CreatedDate = DateTime.ParseExact(I.Fields["PostedDate"].Value, "yyyyMMdd'T'HHmmss'Z'", CultureInfo.InvariantCulture).ToString("dd/MM/yyyy");
+                    var Title = I.Fields["Title"].Value;
+                    var SmallDescription = I.Fields["ShortDiscription"].Value;
+                    var Image = I.Fields["ArticalsSmallImage"].Value;
+
+                    Sitecore.Data.Fields.ImageField imageField = I.Fields["ArticalsSmallImage"];
+                    Sitecore.Data.Items.MediaItem image = new Sitecore.Data.Items.MediaItem(imageField.MediaItem);
+                    string imageSrc = Sitecore.StringUtil.EnsurePrefix('/',
+                    Sitecore.Resources.Media.MediaManager.GetMediaUrl(image));
+                    //var Buttontext = item.Fields["CardButtonText"].Value;
+
+                    //string imgTag = String.Format(@"<img src=""{0}"" alt=""{1}"" />", src, image.Alt);
+
+                    BC.Add(new BlogCard(ID, Title, CreatedDate, SmallDescription, imageSrc, image.Alt));
+                }
+            }
+
+
+
+            if (query != null)
+            {
+                List<BlogCard> results = null;
+
+                results = BC.FindAll(Findtitle);
+
+                bool Findtitle(BlogCard AMp)
+                {
+                    if (AMp.BlogTitle.ToLower().Contains(query.ToLower()) || AMp.ShortDesc.ToLower().Contains(query.ToLower()))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return View("~/Views/ProjectViews/Dynamics.cshtml", results);
+            }
+            return View("~/Views/ProjectViews/Dynamics.cshtml", BC);
+
+        }
+
+    }
+}
---
+++ ProjectSolution1/Models/BlogCard.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSolution1.Models
+{
+    public class BlogCard
+    {
+
+        public string BlogId { get; set; }
+
+        public string BlogTitle { get; set; }
+
+        public string BlogImage { get; set; }
+
+        public string Posteddate { get; set; }
+
+        public string Category { get; set; }
+
+        public string ShortDesc { get; set; }
+        public string imgsrc { get; set; }
+
+        public string imgalt { get; set; }
+
+
+        public BlogCard(String BlogId, String BlogTitle, string Posteddate, string ShortDesc, string imgsrc, string imgalt)
+        {
+
+            this.BlogId = BlogId;
+            this.BlogTitle = BlogTitle;
+            this.BlogImage = BlogImage;
+            this.Posteddate = Posteddate;
+            this.ShortDesc = ShortDesc;
+            this.imgsrc = imgsrc;
+            this.imgalt = imgalt;
+        }
+
+
+
+    }
+}
